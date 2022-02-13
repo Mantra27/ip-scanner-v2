aac1d67259987475820ae1018b31cbb1bc9a444a
Mantra Gohil <75286433+Mantra27@users.noreply.github.com>
2022-02-13 22:13:12 +0530

    Update windows.js

--- windows.js
+++ windows.js
@@ -1,104 +1,144 @@
-const src = (i, j)=>{
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
-const {exec} = require("child_process")
-const int = i;
-const fin = j;
-            //0   1   2  3
-console.log('running')
-let cache_IP = int + "." + fin;
-try{
-    const MOD = async (int, fin) => {
-    const ping = async (e)=>{
-        await exec(`ping ${e} -n 1`, (stderr, stdout) => {
-            if(stderr){
-                console.log(stderr);
-                return "stderr//"
-            }
-            else {
-                console.log('Average ping of '+ e +' is ', stdout.split(":")[5].split('=')[3].trim());
-                
-            }
+const sourceTree = async (firstArgument, secondArgument) => {
+    let list = [];
+    let first_arr = firstArgument.split(".");
+    let second_arr = secondArgument.split(".");
+    let cof = first_arr;
+    let cos = second_arr;
+
+    try{
+        if(first_arr.length < 4|| second_arr.length < 4){
+            console.log("invalid ip addresses");
+        };
+    
+        let ip1 = [];
+
+        const newPromise = new Promise((resolve, reject) => {
+            first_arr.forEach(async (i, low) => {
+                if(i>255){
+                    resolve("invalid ip format, please enter a valid ip address in initial IP address");
+                }
+                else{
+                    ip1[low] = i;
+                }        
+            });
+        });
+        
+        newPromise.then((e)=>{
+            console.log(e)
         })
-    } 
-
-    int = int.split('.')
-    fin = fin.split('.')
-
-    int[0] = parseInt(int[0]);
-        int[1] = parseInt(int[1]);
-            int[2] = parseInt(int[2]);
-                int[3] = parseInt(int[3]);
-
-    fin[0] = parseInt(fin[0]);
-        fin[1] = parseInt(fin[1]);
-            fin[2] = parseInt(fin[2]);
-                fin[3] = parseInt(fin[3]);
-
-    for(let ips = 0; ips<cache_IP.split(".").length; ips++){
-       if(cache_IP.split('.')[ips]>255){
-           return "invalid ips[255]";
-       }
-    }
-
-    //ips availability in position 2;
-    let diffr_host_main = fin[2]-int[2];
-    let diffr_host_sub = fin[3]-int[3];
-    if(int[0] != fin[0] || int[1] != fin[1]){
-        return "too many IP's to scan"
-    }
-    else if(int[2]>fin[2] || int[3] >> fin[3]){
-        if(int[2]>=fin[2]){
-            
-            return "initial value is higher than final"
-        }
-    }
-
-    else{
-
-       
         
-        let temp_ip = int;
-        if(int[2] != fin[2]){
-            diffr_host_sub = 255-int[3];
-        }
-        try{
-            for(let main_counter = 0; main_counter <= diffr_host_main; main_counter++){
-                temp_ip[3]--;
-                for(let sub_counter = 0; sub_counter <= diffr_host_sub; sub_counter++){
-                    let temp_sub_counter = temp_ip;
-                        temp_sub_counter[3]++;
-                    
-                        ping(temp_sub_counter.join('.'))
-                    
+        let ip2 = [];
+
+        const alsonewPromise = new Promise((resolve, reject) => {
+            second_arr.forEach(async (i, low) => {
+                if(i>255){
+                    resolve("invalid ip format, please enter a valid ip address in final IP address");
                 }
-                temp_ip[3] = 0;
-                temp_ip[2]++;
-                int[3] = 0;
-                if(int[2]==fin[2]){
-                    diffr_host_sub =  fin[3]-int[3];
+                else{
+                    ip1[low] = i;
+                }        
+            });
+        });
+        
+        alsonewPromise.then((e)=>{
+            console.log(e)
+        })
+    
+
+        let not_eligible = new Promise((resolve, reject) => {
+    
+            ip1.forEach((element, key) => {
+                if(ip2[key] - ip1[key] < 0 && key != 3){    
+                    resolve("initial ip is higher than final ip range, they will be considered as the opposite ranges in the code")
+
                 }
-                else if(int[2] != fin[2]){
-                    diffr_host_sub = 255;
-                }
-                
+            });
+    
+            if(cos[2] - cof[2] >= 3){
+                resolve("so many ip's to scan");
+            };
+    
+            if(cof[0] != cos[0]){
+                resolve("so many ip's to scan");
             }
-        }
-        catch(e){
-            console.log("err caught", e)
-        }
-        
+
+            if(cof[1] != cos[1]){
+                
+                resolve("so many ip's to scan");
+            }
+    
+        });
+    
+        not_eligible.then(e => {
+            console.log(e);
+        });
+
+        let temp_ip = cof;
+        let diffr_host_sub = cos[3]-cof[3];
+
+            if(cof[2] != cos[2]){
+                diffr_host_sub = 255;
+            }
+            try{
+                for(let main_counter = cof[2]; main_counter <= cos[2]; main_counter++){
+
+                    temp_ip[3]--;
+
+                    for(let sub_counter = cof[3]; sub_counter <= diffr_host_sub; sub_counter++){
+
+                        let temp_sub_counter = temp_ip;
+                            temp_sub_counter[3]++;
+                            list[list.length] = temp_sub_counter.join('.');
+                    }
+
+                    temp_ip[3] = 0;
+                    temp_ip[2]++;
+                    cof[3] = 0;
+
+                    if(cof[2]==cos[2]){
+                        diffr_host_sub =  cos[3]-cof[3];
+                    }
+                    
+                    else if(cof[2] != cof[2]){
+                        diffr_host_sub = 255;
+
+                    }
+
+                }
+            }
             
-            
+            catch(e){
+                console.log("err caught", e)
+            }
+            console.log(list)
+        for(let loop = 0; loop < list.length; loop++){
+
+            async function lsExample() {
+                try {
+                  const { stderr, stdout } = await exec(`ping ${list[loop]} -c 1`);
+
+                  if(stderr){
+                      console.log("stderr: ", stdout);
+                  }
+
+                  console.log(`=====================================================`)
+                  console.log(stdout);
+                } catch (e) {
+                  console.error(e); // should contain code (exit code) and signal (that caused the termination).
+                }
+              }
+              await lsExample()
+
+        }
+    }
+    catch(e){
+        console.log("something went down while scanning ips", e);
     }
     
-    return "success";
-}
-console.log(MOD(int, fin))
-    }
-catch(e){
-    console.log('main catch(MOD catch)', e)
-    }
-}
-    module.exports = {
-        src,
-    }
+
+};
+
+module.exports = sourceTree;
