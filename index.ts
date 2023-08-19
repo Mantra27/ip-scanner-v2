733a1921f90e9c4b73cef95e24221c6bb4f4a12e
Mantra Gohil <mantragohil@mantra-2.local>
2023-08-19 13:41:04 +0530

    bored commit

---
+++ index.ts
@@ -0,0 +1,3 @@
+// const ping = require("./windows"); //for windows
+const ping = require("./script"); //for mac-os
+ping("192.168.0.0", "192.168.0.30");
---
+++ script.ts
@@ -0,0 +1,59 @@
+const readline = require('readline');
+const { exec } = require('child_process');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function pingIP(ip: string): Promise<boolean> {
+  return new Promise<boolean>((resolve) => {
+    const platform = process.platform;
+    let pingCommand = '';
+
+    if (platform === 'win32') {
+      pingCommand = `ping -n 1 ${ip}`;
+    } else if (platform === 'linux' || platform === 'darwin') {
+      pingCommand = `ping -c 1 ${ip} -t 3`;
+    } else {
+      throw new Error('Unsupported platform');
+    }
+
+    exec(pingCommand, (error: string, stdout: string) => {
+      if (error) {
+        resolve(false);
+      } else {
+        // Check the stdout for success indicators (specific to each platform)
+        if (platform === 'win32') {
+          resolve(stdout.includes('Received = 1'));
+        } else if (platform === 'linux' || platform === 'darwin') {
+          resolve(!stdout.includes('Request timeout for'));
+        } else {
+          resolve(false);
+        }
+      }
+    });
+  });
+}
+
+async function scanIPRange(startIP: string, endIP: string) {
+  const startParts = startIP.split('.').map(Number);
+  const endParts = endIP.split('.').map(Number);
+
+  for (let i = startParts[3]; i <= endParts[3]; i++) {
+    const ip = `${startParts[0]}.${startParts[1]}.${startParts[2]}.${i}`;
+    const isReachable = await pingIP(ip);
+    if (isReachable) {
+      console.log(`${ip} is reachable`);
+    } else {
+      console.log(`${ip} is not reachable`);
+    }
+  }
+}
+
+rl.question('Enter starting IP (e.g., 1.1.1.0): ', (startIP: string) => {
+  rl.question('Enter ending IP (e.g., 1.1.1.3): ', (endIP: string) => {
+    scanIPRange(startIP, endIP).catch((error) => console.error(error));
+    rl.close();
+  });
+});
