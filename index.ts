435a1363a4730dd414a03960d15b8ecbf4c870a7
Mantra Gohil <mantragohil@mantra-2.local>
2023-08-19 13:41:59 +0530

    dir changes

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 // const ping = require("./windows"); //for windows
-const ping = require("./script"); //for mac-os
+const ping = require("./scripts/scanner"); //for mac-os
 ping("192.168.0.0", "192.168.0.30");
